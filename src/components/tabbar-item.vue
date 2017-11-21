2a22e02c69c07f27d7fea15d789613ca9467982f
majun <needyousir@qq.com>
2017-11-21 15:44:56 +0800

    tabbar

---
+++ src/components/tabbar-item.vue
@@ -0,0 +1,32 @@
+<template>
+    <a class="m-tabbar-item" >
+        <span class="m-tabbar-item-icon"><slot name="icon-normal"></slot></span>
+        <span class="m-tabbar-item-text"><slot></slot></span>
+    </a>
+</template>
+
+<style lang="less">
+.m-tabbar-item{
+    flex: 1;
+    text-align: center;
+    .m-tabbar-item-icon{
+        display: block;
+        padding-top: 2px;
+        img{
+            width: 28px;
+            height: 28px;
+        }
+
+    }
+    .m-tabbar-item-text{
+        display: block;
+        font-size: 10px;
+        color:#949494;
+    }
+    &.is-active{
+        .m-tabbar-item-text{
+            color: #42bd56;
+        }
+    }
+}
+</style>
---
+++ src/pages/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <m-tabbar>
+      <m-tabbar-item id='tab1'>
+        <img src="../assets/images/ic_tab_home_normal.png" alt="" slot="icon-normal"> 
+        首页
+      </m-tabbar-item>
+      <m-tabbar-item id='tab2'>
+        <img src="../assets/images/ic_tab_subject_normal.png" alt="" slot="icon-normal"> 
+        书影音
+      </m-tabbar-item>
+      <m-tabbar-item id='tab3'>
+        <img src="../assets/images/ic_tab_status_normal.png" alt="" slot="icon-normal"> 
+        广播
+      </m-tabbar-item>
+      <m-tabbar-item id='tab4'>
+      	<img src="../assets/images/ic_tab_group_normal.png" alt="" slot="icon-normal">
+        小组
+      </m-tabbar-item>
+       <m-tabbar-item id='tab5'>
+        <img src="../assets/images/ic_tab_profile_normal.png" alt="" slot="icon-normal"> 
+        我的
+      </m-tabbar-item>
+    </m-tabbar>
+  </div>
+</template>
+
+<script>
+  import mTabbar from '../components/tabbar'
+  import mTabbarItem from '../components/tabbar-item'
+  export default {
+    name: 'index',
+    components: {
+      mTabbar,
+      mTabbarItem
+    }
+  }
+</script>
